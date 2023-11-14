4a6bc8ea376b4001309a1f138dfffa1ae93accdc
GustavGroenborg <gustavrisagerus@gmail.com>
2023-11-14 20:49:45 +0100

    Moved LocateDir test and other stuff

--- dec7/Tests/dec7Tests/ParserTests.swift
+++ dec7/Tests/dec7Tests/ParserTests.swift
@@ -121,4 +121,30 @@ final class ParserTests: XCTestCase {
         // Then
         XCTAssertEqual(expected, output)
     }
+
+
+    func test_locateDirectory() throws {
+        // Given
+        var root = Directory(name: "/")
+        var dirA = Directory(name: "a", parent: root)
+        var dirE = Directory(name: "e", parent: dirA)
+        var dirD = Directory(name: "d", parent: root)
+        var dirF = Directory(name: "f", parent: dirD)
+
+        // When
+        guard let foundDir = locate(directory: "f", in: root) else {
+            return XCTFail("Could not find directory of name \(dirF.name).")
+        }
+
+        // Then
+        XCTAssertTrue(dirF === foundDir)
+    }
+
+
+    func test_parseDirectory() throws {
+        // Given
+        let input = "cd foo"
+
+
+    }
 }
