c3fd61a476cb547c1132a0ab7c7a965a48ffb6d9
GustavGroenborg <gustavrisagerus@gmail.com>
2023-11-15 16:39:13 +0100

    Finished part 2

--- dec7/Sources/main.swift
+++ dec7/Sources/main.swift
@@ -16,7 +16,28 @@ directoriesBelow100_000.forEach { sum += $0.size }
 let fmt = NumberFormatter()
 fmt.numberStyle = .decimal
 
+print("part 1:")
 print("The sum of all directories below 100_000 is: \(fmt.string(for: sum)!)")
+
+
+let neededSpace = parser.root.size + 30_000_000 - 70_000_000
+print("root size: \(fmt.string(for: parser.root.size)!)")
+
+var v: Directory
+var smallestDirectory = parser.root
+var frontier = [parser.root]
+
+while !frontier.isEmpty {
+    v = frontier.removeLast()
+    if v.size >= neededSpace && v.size < smallestDirectory.size {
+        smallestDirectory = v
+    }
+
+    v.directories.forEach { frontier.append($0) }
+}
+
+print("The total size of the smallest directory, that would free up at least: \(fmt.string(for: neededSpace)!) is: \(fmt.string(for: smallestDirectory.size)!)")
+
 
 func loadInput() -> String {
     let fp = "file://" + FileManager.default.homeDirectoryForCurrentUser.path + "/AdventOfCode/aoc_2022/puzzleInputs/dec7.txt"
